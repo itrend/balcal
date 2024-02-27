def7f61c1dd93aaf3b2096479b11ec7c1e396c4f
itrend <itrend@users.noreply.github.com>
2024-02-27 11:19:47 +0200

    list limitations, typos

--- src/components/help.tsx
+++ src/components/help.tsx
@@ -2,17 +2,22 @@ export function Help() {
   return (
     <div id="help">
       <h3>Help</h3>
-      Very simple score calculator for Balatro with many <b>limitations:</b> (non-exclusive list)
+      Very simple hand score calculator for <a href="https://www.playbalatro.com/">Balatro</a> with
+      many <b>limitations:</b> (non-exclusive list)
       <ul>
-        <li>extra chips and mult must be summed manually, more or less</li>
-        <li>retrigger cards must be accounted for manually</li>
+        <li>extra chips and mult must be gathered manually from the game</li>
+        <li>retrigger cards must be accounted for manually, as extra chips</li>
+        <li>mult multiplier is always calculated after mult additions</li>
+        <li>debuffed card&apos;s chips must be manually substracted from extra chips (possibly going negative)</li>
+        <li>the secret combinations (five of a kind, flush house and flush five) are not correct as I
+          do not know their strength</li>
       </ul>
       <hr/>
-      <b>Poker hand</b>: the ranks only of your poker hand, spaces are optional. Each 
+      <b>Poker hand</b>: the ranks only of your poker hand, spaces are optional. Each
       of <code>23456789JQK</code> mean what you expect, <code>1</code> is interpreted
       as <code>A</code>, unless directly followed by a zero, in which case it&apos;s interpreted
-      as <code>10</code>. Use <code>0</code> or <code>S</code> for stone card. 
-      Examples: <code>12345</code> - ace two three four five, <code>10 10 J Q S</code> - 
+      as <code>10</code>. Use <code>0</code> or <code>S</code> for stone card.
+      Examples: <code>12345</code> - ace two three four five, <code>10 10 J Q S</code> -
       ten ten jack queen stone-card, <code>10JQK1</code> - ten jack queen king ace
       <br />
       <b>Flush</b>: colors only matter for flushes, so instead of telling me all your colors,
@@ -30,9 +35,11 @@ export function Help() {
       <b>Extra mult</b>: extra mult *additions* from jokers, retriggers, and other sources. As above,
       you can type more than one number and they will be summed
       <br />
-      <b>Mult mult chain</b>: all of your multuplier multipliers. A list of numbers that will be multiplied
+      <b>Mult mult chain</b>: all of your multiplier multipliers. A list of numbers that will be multiplied
       together to get the final multiplier e.g. <code>1.5 2 3</code> will yield final multiplier of <code>9</code>
       <br />
+      <a href="https://github.com/itrend/balcal">source code</a>
+      <a href="https://github.com/itrend/balcal/issues">issues</a>
     </div>
   )
 }
